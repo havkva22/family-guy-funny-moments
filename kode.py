1cf279b94419e0ef17401bed76b1f5c2cd9cea37
havkva22 <118523492+havkva22@users.noreply.github.com>
2022-11-22 09:30:29 +0100

    Update kode.py
    ny kode

--- kode.py
+++ kode.py
@@ -1,1 +1,12 @@
 #tom kode
+class Bil:
+    def __init__(self, regnum, merke, modell, eigar):
+        self.regnum = regnum
+        self.merke = merke
+        self.modell = modell
+        self.eigar = eigar
+class Passering:
+    def __init__(self, dato, time, regnum):
+        self.dato = dato
+        self.time = time
+        self.regnum = regnum
